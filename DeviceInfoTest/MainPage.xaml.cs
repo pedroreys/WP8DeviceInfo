eccec8721c93b71f161d94e12da818c8b2fbd9ab
Pedro Reys <pedroreys@gmail.com>
2013-06-26 15:20:08 -0500

    get device related info

---
+++ DeviceInfoTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using DeviceInfoTest.Resources;
+
+namespace DeviceInfoTest
+{
+    using System.Windows.Input;
+
+    using Microsoft.Phone.Info;
+    using Microsoft.Phone.Net.NetworkInformation;
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MnoName.Text = "MNO Name: " + DeviceNetworkInformation.CellularMobileOperator;
+            HandsetManufacturer.Text = "Handset Manufacturer: " + DeviceStatus.DeviceManufacturer;
+            ActualOsVersion.Text = "Actual OS Version: " + Environment.OSVersion.Version;
+            HandsetModel.Text = "HandsetModel: " + DeviceStatus.DeviceName;
+        }
+    }
+}
